9a5d3ff810f16d08d0946b83da0c07032bf05c58
SPyd3rByt3 <DailyDoseOfSDGKU@gmail.com>
2024-10-07 20:47:06 -0700

    extra practice

--- LOOPS.js
+++ LOOPS.js
@@ -1,6 +1,70 @@
-const i = 2
+/*const is 2 (in *Table of 2's) (or whatever num is put into the prompt so as to always be multiplied by itself = same num)
 
-var f = 0      //1-10 or 0???
+var is 0-10 or 0???*/
+
+//document.write("<h2> For Loops Practice");
+
+    //ACTIVITY 1//  <<---NOT EFFICIENT --- too much small memory!!!
+    //Like if you needed to write 2 * 0-100, it wouldl be obsolete & redundant.
+    //Dont use EVEN IF EFFECTIVE
+
+/*document.write(`
+    <h3> Table of 2's!<h3>
+    //</br> -->unneccessary
+    <p>2 x 0 = 0<6/p>
+    <p>2 x 1 = 2</p>
+    <p>2 x 2 = 4</p>
+    <p>2 x 3 = 6</p>
+    <p>2 x 4 = 8</p>
+    <p>2 x 5 = 10</p>
+    <p>2 x 6 = 12</p>
+    <p>2 x 7 = 14</p>
+    <p>2 x 8 = 16</p>
+    <p>2 x 9 = 18</p>
+    <p>2 x 10 = 20</p>
+`);*/
+
+
+///THE 'FOR' LOOP\\\
+
+/*for(starting_value; stopping_value; interval){
+    //activities
+} 
+
+
+for(let i=0; i<11; i++){ 
+    console.log("using for loops");
+    
+}*/
+
+//you can declare the const or index=0 BEFORE the FOR statemnent
+//for(let index=0; index<11; index++){ 
+//   console.log("using for loops");
+//}
+
+//index++ = index + 1 (adding 1 each time)
+//for (let index = 0; index < array.length; index++) {
+//    const element = array[index];
+//   for
+//}
+
+
+
+// Activity 3 JUst does 1 2 3 4 5...//
+
+//for (let i = 0; i<=10; i++){
+//    let result = 2*i; 
+    //console.log(`<p>2 x {i} = {result}</p>`);
+//    document.write(`<p> 2 x ${i} = ${result} </p>`);  
+//}
+
+//ACtivity 4
+for(let i=2; i<101; i+=2){
+    document.write(`<p>${i}</p>`) //or do a console.log(``)
+}
+
+
+
 
 
 let number = prompt("Enter the number of the table you want to display: ")
